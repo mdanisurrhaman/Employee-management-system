77e02602f3808ee8c97abcc1bbc65c4ca28af96f
Md Anisurrhaman <166200118+mdanisurrhaman@users.noreply.github.com>
2025-02-17 23:47:31 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,24 +1,24 @@
+"""
+URL configuration for test_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path
-from test_app import views
+from django.urls import path,include
 
 urlpatterns = [
-    path('home/', views.home),
-    path('index/', views.index),
-    path('getdata/',views.getdata, name='getdata'),
-    path('about/', views.about),
-    path('addData/', views.addData,name='addData'),
-    path('updateData/<int:id>',views.updateData,name='updateData'),
-    path('Delete/<int:id>',views.Delete,name='Delete'),
-
-    path('Students/',views.Students,name='Students'),
-    path('Addstudent/', views.Addstudent, name='Addstudent'),
-    path('updatestudent/<int:id>/', views.updatestudent, name='updatestudent'),
-    path('deletes/<int:id>/', views.deletes, name='deletes'),
-    path('contact/', views.contact,name='contact'),
-    path('register/',views.register),
-    path('login/',views.login_view,name='login'),
-    path('logout/',views.logout_view,name='logout')
-
+    path('admin/', admin.site.urls),
+    path('', include('test_app.urls')),
 
 ]
